034592f94d5b5821f11c9f58b8070a9a3b9c8541
Mark Yafi <mark.yafi@outlook.com>
2026-02-03 20:43:55 -0600

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Mark on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
