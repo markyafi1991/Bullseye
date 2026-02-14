3adc3f49eb8f161657b0ca64175776c9f7cf37ea
Mark Yafi <mark.yafi@outlook.com>
2026-02-14 15:34:55 -0600

    This marks the completion of the Kodeco course Your First iOS & SwiftUI App: An App from Scratch.

---
+++ Bullseye/Views/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Mark on 2/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var alertIsVisible = false
+    @State private var sliderValue = 50.0
+    @State private var game = Game()
+    
+    
+    var body: some View {
+        Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+            .bold()
+            .multilineTextAlignment(.center)
+            .lineSpacing(4.0)
+            .font(.footnote)
+            .kerning(2.0)
+        Text(String(game.target))
+            .kerning(-1.0)
+            .font(.largeTitle)
+            .fontWeight(.black)
+        HStack {
+            Text("1")
+                .bold()
+            Slider(value: $sliderValue, in: 1.0...100.0)
+            Text("100")
+                .bold()
+        }
+        Button("Hit me") {
+            alertIsVisible = true
+        }
+        .alert(
+            "Hello there!",
+            isPresented: $alertIsVisible,
+            actions: {
+                Button("Awesome") {
+                    print("Alert closed")
+                }
+            },
+            message: {
+                let roundedValue = Int(sliderValue.rounded())
+                Text( """
+                    The slider's value is \(roundedValue).
+                    You scored \(game.points(sliderValue: roundedValue)) points this round.
+                """)
+            }
+        )
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
+
---
+++ BullseyeTests/BullseyeTests.swift
@@ -0,0 +1,33 @@
+//
+//  BullseyeTests.swift
+//  BullseyeTests
+//
+//  Created by Mark on 2/10/26.
+//
+
+import XCTest
+@testable import Bullseye
+
+final class BullseyeTests: XCTestCase {
+    var game: Game!
+    
+    override func setUpWithError() throws {
+        game = Game()
+    }
+    
+    override func tearDownWithError() throws {
+        game = nil
+    }
+    
+    func testScorePositive() {
+        let guess = game.target + 5
+        let score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 95)
+    }
+    
+    func testScoreNegative() {
+        let guess = game.target - 5
+        let score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 95)
+    }
+}
